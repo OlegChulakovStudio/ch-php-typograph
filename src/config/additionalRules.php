13b5fe5038bf504010d6e0dcd1d87a9af82c9a98
anastasiya.shamsudinova <anastasiya.shamsudinova@chulakov.ru>
2021-11-18 20:11:46 +0300

    Правки правил. доработки

--- src/config/additionalRules.php
+++ src/config/additionalRules.php
@@ -27,12 +27,21 @@ return [
             'replacement' => '\1&nbsp;\3'
         ],
     ],
-// пробел после предлога \ союза, обернутого в тег(-и)
+    // пробел после предлога \ союза, обернутого в тег(-и)
     [
         'selector' => 'Nobr',
         'ruleName' => 'space_after_short_word_in_tag',
         'params' => [
             'pattern' => '/(\s+|\&nbsp\;|\>)([a-zа-яё]{1,2})((\<\/[a-zа-яё0-9\=]+\>)+)(\s+|\&nbsp\;)([a-zа-яё0-9\-]|\<|\>|\&laquo\;)/ieu',
+            'replacement' => '$m[1] . $m[2] . $m[3] . "&nbsp;" . $m[6]',
+        ]
+    ],
+    // пробел после предлога \ союза, за которым следует слово, обернутое в тег(-и)
+    [
+        'selector' => 'Nobr',
+        'ruleName' => 'space_before_word_in_tag',
+        'params' => [
+            'pattern' => '/(\s+|\&nbsp\;|^)([a-zа-яё]{1,2})((\<[a-zа-яё0-9\=]+\>)+)(\s+|\&nbsp\;)([a-zа-яё0-9\-]|\<|\>|\&laquo\;)/ieu',
             'replacement' => '$m[1] . $m[2] . $m[3] . "&nbsp;" . $m[6]',
         ]
     ]
